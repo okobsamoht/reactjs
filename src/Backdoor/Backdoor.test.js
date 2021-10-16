724aef5b0e367c50f09a9bcb80e325d4fb7cd65e
Thomas BOKO <thomas@right-com.com>
2021-10-16 11:00:52 +0100

    updates

--- src/Backdoor/Backdoor.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Backdoor from './Backdoor';
-
-test('verify component', () => {
-	const { getByText } = render(<Backdoor />);
-	const linkElement = getByText(/Backdoor/i);
-	expect(linkElement).toBeInTheDocument();
-});
