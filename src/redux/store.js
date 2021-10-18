da2d5d7096724424800b98b114d4d54d6c7fab31
Thomas BOKO <thomas@right-com.com>
2021-10-18 16:02:00 +0100

    update sore enhancers

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,12 @@ import {applyMiddleware, compose, createStore} from "redux";
 import rootReducer from "./reducers";
 import thunk from "redux-thunk";
 
-const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true, traceLimit: 25 }) || compose);
+const storeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
 
 const store = createStore(
     rootReducer,
